440c2511d26c0582b85bc9e77aacc0ce059adfa0
Yorick Holkamp <github@nextpulse.nl>
2017-06-22 10:43:19 +0200

    Updating the Invoice model to use camel cased properties

--- src/main/java/net/nextpulse/jacumulus/requests/models/Invoice.kt
+++ src/main/java/net/nextpulse/jacumulus/requests/models/Invoice.kt
@@ -2,31 +2,40 @@ package net.nextpulse.jacumulus.requests.models
 
 import net.nextpulse.jacumulus.util.typeadapters.DateAdapter
 import net.nextpulse.jacumulus.util.typeadapters.PaymentStatusEnumAdapter
-
+import java.util.*
 import javax.xml.bind.annotation.XmlElement
 import javax.xml.bind.annotation.XmlRootElement
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
-import java.util.Date
 
 @XmlRootElement(name = "invoice")
-data class Invoice (
-    var emailaspdf: Emailaspdf? = null,
-    var descriptiontext: String? = null,
+data class Invoice(
+    @get:XmlElement(name = "emailaspdf")
+    var emailAsPdf: Emailaspdf? = null,
+    @get:XmlElement(name = "descriptiontext")
+    var descriptionText: String? = null,
     var template: String? = null,
-    var concepttype: String? = null,
+    @get:XmlElement(name = "concepttype")
+    var conceptType: String? = null,
     var concept: String? = null,
     @get:XmlJavaTypeAdapter(PaymentStatusEnumAdapter::class)
-    var paymentstatus: PaymentStatus? = null,
-    var invoicenotes: String? = null,
+    @get:XmlElement(name = "paymentstatus")
+    var paymentStatus: PaymentStatus? = null,
+    @get:XmlElement(name = "invoicenotes")
+    var invoiceNotes: String? = null,
     @get:XmlElement(name = "line")
     var lines: List<Line>? = null,
     var number: String? = null,
     @get:XmlJavaTypeAdapter(DateAdapter::class)
-    var paymentdate: Date? = null,
-    var vattype: String? = null,
+    @get:XmlElement(name = "paymentdate")
+    var paymentDate: Date? = null,
+    @get:XmlElement(name = "vattype")
+    var vatType: String? = null,
     var description: String? = null,
     @get:XmlJavaTypeAdapter(DateAdapter::class)
-    var issuedate: Date? = null,
-    var costcenter: String? = null,
-    var accountnumber: String? = null
+    @get:XmlElement(name = "issuedate")
+    var issueDate: Date? = null,
+    @get:XmlElement(name = "costcenter")
+    var costCenter: String? = null,
+    @get:XmlElement(name = "accountnumber")
+    var accountNumber: String? = null
 )
